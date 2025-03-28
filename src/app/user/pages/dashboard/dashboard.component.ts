e88eb1754b8f1321391e1d589046c9f3fd7e8675
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-28 13:38:41 +0530

    feat : Implemented User Dashboard and Product List Pages.

--- src/app/user/pages/dashboard/dashboard.component.ts
+++ src/app/user/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  // Injecting the necessary dependencies
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  // This function is invoked when the user clicks on the show products list page
+  onProductListPageClick() {
+    this.router.navigate(['../', 'products'], { relativeTo: this.route });
+  }
+
+  // This function is invoked when the user clicks on the show cart page
+  onCartPageClick() {
+    this.router.navigate(['../', 'cart'], { relativeTo: this.route });
+  }
+
+  // This function is invoked when the user clicks on the show order list page
+  onOrderListPageClick() {
+    this.router.navigate(['../', 'orders'], { relativeTo: this.route });
+  }
+}
--- src/app/user/pages/products/products.component.ts
+++ src/app/user/pages/products/products.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { ProductDto } from 'src/app/shared/models/product/ProductDto';
+import { ProductService } from 'src/app/shared/services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  // These are the data for this component
+  productList: ProductDto[] = [];
+
+  // Injecting the necessary dependencies
+  constructor(private productService: ProductService) {}
+
+  // Initial Api calls are done
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  // This function Fetches the products
+  fetchProducts() {
+    this.productService.fetchAllProducts().subscribe({
+      next: (productList) => (this.productList = productList),
+    });
+  }
+}
